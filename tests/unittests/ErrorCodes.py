b8c285e76c2e2686b7360cc889ffc52558f1ac91
Anish <anishasrani@gmail.com>
2019-06-08 18:31:26 -0700

    updated unit tests to match error codes

--- tests/unittests/ErrorCodes.py
+++ tests/unittests/ErrorCodes.py
@@ -8,16 +8,16 @@ from storyscript.ErrorCodes import ErrorCodes
     ('unidentified_error', ('E0001', '')),
     ('service_name', ('E0002', "A service name can't contain `.`")),
     ('arguments_noservice', (
-        'E0003', 'You have defined an argument, but not a service'
+        'E0003', 'Unefined argument; not a service'
     )),
     ('return_outside', ('E0004', '`return` is allowed only inside functions')),
     ('variables_backslash', ('E0005', "A variable name can't contain `/`")),
     ('variables_dash', ('E0006', "A variable name can't contain `-`")),
     ('assignment_incomplete', ('E0007', 'Missing value after `=`')),
-    ('function_misspell', ('E0008', 'You have misspelt `function`')),
-    ('import_misspell', ('E0009', 'You have misspelt `import`')),
+    ('function_misspell', ('E0008', 'Misspelt `function`')),
+    ('import_misspell', ('E0009', 'Misspelt `import`')),
     ('import_misspell_as', ('E0010',
-                            'You have misspelt `as` in an import statement')),
+                            'Misspelt `as` in an import statement'')),
     ('import_unquoted_file', ('E0011',
                               'The imported filename must be in quotes')),
     ('string_opening_quote', ('E0012', 'Missing opening quote for string')),
