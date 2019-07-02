00eb222ba57c75971e7439f742b1fdd1ec5386e8
Anish Asrani <anishasrani@gmail.com>
2019-07-02 00:59:08 -0700

    Update storyscript/ErrorCodes.py
    Co-Authored-By: Sebastian Wilzbach <seb@wilzba.ch>

--- storyscript/ErrorCodes.py
+++ storyscript/ErrorCodes.py
@@ -6,7 +6,7 @@ class ErrorCodes:
     unidentified_error = ('E0001', '')
     service_name = ('E0002', "A service name can't contain `.`")
     arguments_noservice = ('E0003',
-                           'Unefined argument; not a service')
+                           'Undefined argument; not a service')
     return_outside = ('E0004', '`return` is allowed only inside functions')
     variables_backslash = ('E0005', "A variable name can't contain `/`")
     variables_dash = ('E0006', "A variable name can't contain `-`")
